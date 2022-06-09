34d9f819480ae177e3587df069bbc77da3494dab
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2022-06-09 01:20:39 -0300

    binary tree lesson completed

--- Jovian/Lesson_2 - Binary Search Trees, Traversals and Recursion/binary_trees.py
+++ Jovian/Lesson_2 - Binary Search Trees, Traversals and Recursion/binary_trees.py
@@ -292,4 +292,212 @@ class TreeNodeFull():
             node = TreeNode(data)
         return node
 
+# Binary Search Tree (BST)
 
+# A binary search tree or BST is a binary tree that satisfies the
+# following conditions:
+
+# 1.The left subtree of any node only contains nodes with keys less than the
+# node's key.
+# 2. The right subtree of any node only contains nodes with keys
+# greater than the node's key.
+
+# It follows from the above conditions that every subtree of a binary search
+# tree must also be a binary search tree.
+
+# QUESTION 8: Write a function to check if a binary tree is a binary search
+# tree (BST).
+
+# QUESTION 9: Write a function to find the maximum key in a binary tree.
+
+# QUESTION 10: Write a function to find the minimum key in a binary tree.
+
+# pylint: disable=missing-function-docstring
+
+
+def remove_none(nums):
+    return [x for x in nums if x is not None]
+
+
+def is_bst(node):
+    if node is None:
+        return True, None, None
+
+    is_bst_l, min_l, max_l = is_bst(node.left)
+    is_bst_r, min_r, max_r = is_bst(node.right)
+
+    is_bst_node = (is_bst_l and is_bst_r and
+                   (max_l is None or node.key > max_l) and
+                   (min_r is None or node.key < min_r))
+
+    min_key = min(remove_none([min_l, node.key, min_r]))
+    max_key = max(remove_none([max_l, node.key, max_r]))
+
+    # print(node.key, min_key, max_key, is_bst_node)
+
+    return is_bst_node, min_key, max_key
+
+
+tree1 = TreeNodeFull.parse_tuple(
+    ((1, 3, None), 2, ((None, 3, 4), 5, (6, 7, 8))))
+print(is_bst(tree1))
+
+tree2 = TreeNodeFull.parse_tuple(
+    (('aakash', 'biraj', 'hemanth'), 'jadhesh', ('siddhant', 'sonaksh', 'vishal')))
+print(is_bst(tree2))
+
+
+class BSTNode():
+    def __init__(self, key, value=None):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+# QUESTION 11: Write a function to insert a new node into a BST.
+
+
+def insert(node, key, value):
+    if node is None:
+        node = BSTNode(key, value)
+    elif key < node.key:
+        node.left = insert(node.left, key, value)
+        node.left.parent = node
+    elif key > node.key:
+        node.right = insert(node.right, key, value)
+        node.right.parent = node
+    return node
+
+
+bsdtree = insert(None, edu.username, edu)
+insert(bsdtree, test.username, test)
+insert(bsdtree, user10.username, user10)
+
+display_keys(bsdtree)
+print(tree_height(bsdtree))
+
+
+# QUESTION 11: Find the value associated with a given key in a BST.
+
+def find(node, key):
+    if node is None:
+        return None
+    if key == node.key:
+        return node
+    if key < node.key:
+        return find(node.left, key)
+    if key > node.key:
+        return find(node.right, key)
+
+
+bsdnode = find(bsdtree, 'edu')
+print(bsdnode.key, bsdnode.value)
+
+# QUESTION 12: Write a function to update the value associated with a given
+# key within a BST
+
+
+def update(node, key, value):
+    target = find(node, key)
+    if target is not None:
+        target.value = value
+
+
+update(bsdtree, 'test', User('testUpdate',
+       'test update name', 'testUpdate@example.com'))
+#node = find(bsdtree, 'test')
+# print(node.value)
+
+# QUESTION 13: Write a function to retrieve all the key-values pairs stored in
+# a BST in the sorted order of keys.
+
+
+def list_all(node):
+    if node is None:
+        return []
+    return list_all(node.left) + [(node.key, node.value)] + list_all(node.right)
+
+
+print(list_all(bsdtree))
+
+
+# Balanced Binary Trees
+
+# QUESTION 14: Write a function to determine if a binary tree is balanced.
+
+def is_balanced(node):
+    if node is None:
+        return True, 0
+    balanced_l, height_l = is_balanced(node.left)
+    balanced_r, height_r = is_balanced(node.right)
+    balanced = balanced_l and balanced_r and abs(height_l - height_r) <= 1
+    height = 1 + max(height_l, height_r)
+    return balanced, height
+
+
+print('Is balanced?', is_balanced(bsdtree))
+
+# Balanced Binary Search Trees
+# QUESTION 15: Write a function to create a balanced BST from a sorted
+# list/array of key-value pairs.
+
+
+def make_balanced_bst(data, lo=0, hi=None, parent=None):
+    if hi is None:
+        hi = len(data) - 1
+    if lo > hi:
+        return None
+
+    mid = (lo + hi) // 2
+    key, value = data[mid]
+
+    root = BSTNode(key, value)
+    root.parent = parent
+    root.left = make_balanced_bst(data, lo, mid-1, root)
+    root.right = make_balanced_bst(data, mid+1, hi, root)
+
+    return root
+
+
+data = [(user.username, user) for user in users]
+tree_new = make_balanced_bst(data)
+display_keys(tree_new)
+print('Is balanced?', is_balanced(tree_new))
+
+# Balancing an Unbalanced BST
+# QUESTION 16: Write a function to balance an unbalanced binary search tree.
+
+
+def balance_bst(node):
+    return make_balanced_bst(list_all(node))
+
+
+# A Python-Friendly Treemap]
+
+# We are now ready to return to our original problem statement.
+
+class TreeMap():
+    def __init__(self):
+        self.root = None
+
+    def __setitem__(self, key, value):
+        node = find(self.root, key)
+        if not node:
+            self.root = insert(self.root, key, value)
+            self.root = balance_bst(self.root)
+        else:
+            update(self.root, key, value)
+
+    def __getitem__(self, key):
+        node = find(self.root, key)
+        return node.value if node else None
+
+    def __iter__(self):
+        return (x for x in list_all(self.root))
+
+    def __len__(self):
+        return tree_size(self.root)
+
+    def display(self):
+        return display_keys(self.root)
